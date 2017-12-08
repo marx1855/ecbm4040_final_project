c0100c272acfa3b171e9b7de9e48d338e1400723
Mingyuan Dong <md3487@columbia.edu>
2017-12-08 14:27:45 -0500

    added serveral functions for preprocessing

--- data_processing/metadata_processing.py
+++ data_processing/metadata_processing.py
@@ -29,4 +29,43 @@ def get_metadata(filepath):
         f[item[0]].visititems(print_attrs)
         
     return metadata
+
+
+def alter(data):
+    metadata_dict = {}
+    for i in range(0, len(data['label'])):
+        metadata_dict[str(i + 1)] = {}
+        metadata_dict[str(i + 1)]['label'] = data['label'][i]
+        metadata_dict[str(i + 1)]['top'] = data['label'][i]
+        metadata_dict[str(i + 1)]['left'] = data['label'][i]
+        metadata_dict[str(i + 1)]['width'] = data['label'][i]
+        metadata_dict[str(i + 1)]['height'] = data['label'][i]
+    
+    return metadata_dict
+
+def extend_label(data):
+    for i in range(0, len(data['label'])):
+        data['label'][i].insert(0, len(data['label'][i]))
+        while len(data['label'][i]) < 6:
+            data['label'][i].append(0)
+    
+    return data
+
+def get_digit_border(metadata):
+    ret = []
+    for i in range(0, len(metadata['label'])):
+        top = min(metadata['top'][i])
+        left = min(metadata['left'][i])
+        bot = 0
+        right = 0
+        for idx in range(len(metadata['top'][i])):
+            bot = max(bot, metadata['height'][i][idx] + metadata['top'][i][idx])
+            right = max(right, metadata['left'][i][idx] + metadata['width'][i][idx])
         
+        ret.append([top, bot, left, right])
+    
+    return ret
+        
+            
+            
+    
